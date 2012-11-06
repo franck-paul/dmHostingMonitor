7e26c7bf333078f92975f00376675ac673f8c0e9
franck <carnet.franck.paul@gmail.com>
2012-11-06 11:16:52 +0100

    Revert to original name and desc in define.php of plugins. Localized versions are not required here.

--- _define.php
+++ _define.php
@@ -12,8 +12,8 @@
 if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
-	/* Name */				__("Hosting Monitor Dashboard Module"),
-	/* Description*/		__("Display server information on dashboard"),
+	/* Name */			"Hosting Monitor Dashboard Module",
+	/* Description*/		"Display server information on dashboard",
 	/* Author */			"Franck Paul",
 	/* Version */			'0.4'
 );
