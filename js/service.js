80c1514fb8a89537cbf94a5fc905705a8ed15333
franck <carnet.franck.paul@gmail.com>
2020-10-06 08:38:50 +0200

    Better display of offline server

--- js/service.js
+++ js/service.js
@@ -21,13 +21,13 @@ dotclear.dmHostingMonitorPing = function() {
       // console.log($('rsp',data).attr('message'));
       // window.console.log('Dotclear REST server error');
       // Server offline
-      $('#content h2').css('filter', 'grayscale(1)');
+      $('body').css('filter', 'grayscale(1)');
       if (!$page.length) {
         $img.prop('alt', dotclear.dmHostingMonitor_Alt + dotclear.dmHostingMonitor_Offline + ` (${new Date().toLocaleString()})`);
       }
     } else {
       // Server online
-      $('#content h2').css('filter', 'none');
+      $('body').css('filter', '');
       if (dotclear && dotclear.data && dotclear.data.darkMode) {
         $img.css('filter', 'brightness(2)');
       } else {
