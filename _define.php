263d41b26013f7642cec0b1730de0bd104e04ac3
franck <carnet.franck.paul@gmail.com>
2023-10-25 19:30:45 +0200

    New release 4.1

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Hosting Monitor Dashboard Module',
     'Display server information on dashboard',
     'Franck Paul',
-    '4.0',
+    '4.1',
     [
         'requires' => [
             ['core', '2.28'],
